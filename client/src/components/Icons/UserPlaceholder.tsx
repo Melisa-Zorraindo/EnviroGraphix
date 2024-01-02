390a24e4d70eb9a6d3a00f2a0405732eea7126f7
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2024-01-02 01:05:38 +0100

    Updates UserPlaceholder size

--- client/src/components/Icons/UserPlaceholder.tsx
+++ client/src/components/Icons/UserPlaceholder.tsx
@@ -1,8 +1,8 @@
 export function UserPlaceholder(): JSX.Element {
   return (
     <svg
-      width={36}
-      height={36}
+      width={30}
+      height={30}
       viewBox="0 0 48 48"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
